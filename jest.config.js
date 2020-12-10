22f766e20dfe79517482b93f50389352b88287d4
williamrepo <47610551+williamrepo@users.noreply.github.com>
2020-12-10 18:17:06 -0300

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  // define o diretorio raiz
+  roots: ['<rootDir>/src'],
+  // indica quais arquivos estão cobertos por testes
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
+  ],
+  // vai gerar uma pasta coverage na raiz do projeto
+  coverageDirectory: 'coverage',
+  setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/src/main/test/cypress'
+  ],
+  testEnvironment: 'jsdom',
+  // converte todos os arquivos em javscript antes de testar usando a lib ts-jest
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
+}
